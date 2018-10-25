76c6be559a0e0b34acb96f9d71f2100b3f3f8990
Slimer <trillapeches@gmail.com>
2018-10-25 19:25:48 -0400

    Updates

---
+++ precache-manifest.1cbfb861a6367beb33a90791c3b850d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d0fc7e0249501552cce",
+    "url": "/partyupfrontend/static/js/runtime~main.1d0fc7e0.js"
+  },
+  {
+    "revision": "fc90e41d5e34d7cdd756",
+    "url": "/partyupfrontend/static/js/main.fc90e41d.chunk.js"
+  },
+  {
+    "revision": "6c2dd97c423f5dda0d1e",
+    "url": "/partyupfrontend/static/js/1.6c2dd97c.chunk.js"
+  },
+  {
+    "revision": "fc90e41d5e34d7cdd756",
+    "url": "/partyupfrontend/static/css/main.05294c62.chunk.css"
+  },
+  {
+    "revision": "09752dc6a6d2d32a8226466f2233fb19",
+    "url": "/partyupfrontend/index.html"
+  }
+];
